d2ba8ec63a85f8b9afcbdf1d22fb2fc48350160a
NIODER <nioder125@gmail.com>
2023-07-14 18:01:59 +0300

    change program template

--- website/core/YCore/Temporary/Program.cs
+++ website/core/YCore/Temporary/Program.cs
@@ -1,46 +1,5 @@
-﻿using System.Globalization;
-using System.Text.Json;
-using Temporary;
-using YApi;
-using YApiModel.Models;
+﻿using YApi;
 
 var client = new YClient("token");
 
-var inputGroups = new List<GroupFillData>()
-            {
-                new()
-                {
-                    Group = 1,
-                    PlayerId = 1
-                },
-                new()
-                {
-                    Group = 1,
-                    PlayerId = 2
-                }
-            };
-
-Console.WriteLine(JsonSerializer.Serialize(inputGroups));
-
-client.GroupFillAsync(inputGroups).Wait();
-var players = client.PlayersGetAsync().Result;
-var player1 = players.First(p => p.Id == 1);
-var player2 = players.First(p => p.Id == 2);
-
-var groups = await client.GroupGetGamesAsync();
-
-Console.WriteLine("________________");
-
-foreach (var g in groups)
-{
-    Console.WriteLine();
-    Console.WriteLine($"Group {g.Group}");
-    Console.WriteLine($"PlayerId {g.PlayerId}");
-    Console.WriteLine();
-}
-
-Console.WriteLine(JsonSerializer.Serialize(groups));
-
-Console.WriteLine("________________");
-
-var group = groups.First(g => g.Group == player1.GroupNumber);
+await client.DeleteLinksAsync(5);
