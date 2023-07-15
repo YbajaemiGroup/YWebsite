057048b105280b90cecf4ad7fd88d1f67586e598
NIODER <nioder125@gmail.com>
2023-07-15 14:22:22 +0300

    Добавлено api для получения списка всех изображений

---
+++ website/core/YCore/YCore/API/HandlerFactories/ImagesListHandlerFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using YCore.API.Handlers;
+
+namespace YCore.API.HandlerFactories
+{
+    public class ImagesListHandlerFactory : HandlerFactory, IHandlerFactory
+    {
+        public ImagesListHandlerFactory(HttpListenerContext context) : base(context)
+        {
+        }
+
+        public IHandler GetHandler()
+        {
+            return new ImagesListHandler();
+        }
+    }
+}
---
+++ website/core/YCore/YCore/API/Handlers/ImagesListHandler.cs
@@ -0,0 +1,18 @@
+﻿using YApiModel.Models;
+using YCore.API.IO;
+using YCore.Data;
+
+namespace YCore.API.Handlers
+{
+    public class ImagesListHandler : IHandler
+    {
+        public IResponseSender GetResponseSender()
+        {
+            var db = DatabaseInteractor.Instance();
+            return new JsonResponseSender(new YApiModel.Response()
+            {
+                ResponseData = db.GetPlayersImages().Select(i => new Image(i.Id, i.ImageName))
+            });
+        }
+    }
+}
