c0f7ba22ab9abbef9f813e8b24b49bbabc5570ed
devokius <carier1313@gmail.com>
2023-07-15 13:00:12 +0300

    Исправления по фронтенду
    Кнопки nav, квадратик nav

--- website/front/js/app.js
+++ website/front/js/app.js
@@ -1,46 +1,43 @@
-const sections = document.querySelectorAll("section");
-const bubble = document.querySelector(".bubble");
-const gradients = [
-    "linear-gradient(to right top, #9af801, #5F01F8) 1",
-    "linear-gradient(to right top, #DCE35B, #45B649) 1",
-    "linear-gradient(to right top, #c0c0aa, #1cefff) 1",
-    "linear-gradient(to right top, #00F260, #0575E6) 1"
-];
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll("section");
+    const bubble = document.querySelector(".bubble");
+    const border_color = "rgba(127, 255, 0, 1)";
 
-const options = {
-    threshold: 0.7
-};
+    const options = {
+        threshold: 0.6
+    };
 
-let observer = new IntersectionObserver(navCheck, options);
+    let observer = new IntersectionObserver(navCheck, options);
 
-function navCheck(entries) {
-    try {
-        entries.forEach(entry => {
-            const className = entry.target.className;
-            const activeAnchor = document.querySelector(`[data-page=${className}]`);
-            // const gradientIndex = entry.target.getAttribute('data-index');
-            const coords = activeAnchor.getBoundingClientRect();
-            const directions = {
-                height: coords.height,
-                width: coords.width,
-                top: coords.top,
-                left: coords.left
-            };
-            if(entry.isIntersecting) {
-                bubble.style.setProperty('left', `${directions.left}px`);
-                bubble.style.setProperty('top', `${directions.top}px`);
-                bubble.style.setProperty('width', `${directions.width}px`);
-                bubble.style.setProperty('height', `${directions.height}px`);
-                // bubble.style.borderImage = gradients[gradientIndex];
-    
-            }
-        });
+    function navCheck(entries) {
+        try {
+            entries.forEach(entry => {
+                const className = entry.target.className;
+                const activeAnchor = document.querySelector(`[data-page=${className}]`);
+                // const gradientIndex = entry.target.getAttribute('data-index');
+                const coords = activeAnchor.getBoundingClientRect();
+                const directions = {
+                    height: coords.height,
+                    width: coords.width,
+                    top: coords.top,
+                    left: coords.left
+                };
+                if(entry.isIntersecting) {
+                    bubble.style.setProperty('left', `${directions.left}px`);
+                    bubble.style.setProperty('top', `${directions.top}px`);
+                    bubble.style.setProperty('width', `${directions.width}px`);
+                    bubble.style.setProperty('height', `${directions.height}px`);
+                    bubble.style.borderColor = border_color;
+                
+                }
+            });
+        }
+        catch (err) {
+        }
+
     }
-    catch (err) {
-    }
-    
-}
 
-sections.forEach(section => {
-    observer.observe(section);
-})
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
--- website/front/js/button.js
+++ website/front/js/button.js
@@ -4,5 +4,38 @@ function scrollToTop() {
         left: 0, 
         behavior: 'smooth'
     });
-}
+};
+
+function scrollToGroup() {
+    const groupSection = document.querySelector('#groupStage');
+    var rect1 = groupSection.getBoundingClientRect();
+    console.log(rect1.top);
+    window.scroll({
+        top: rect1.top + window.scrollY, 
+        left: 0, 
+        behavior: 'smooth'
+    });
+};
+
+function scrollToPlayoff() {
+    const playoffSection = document.querySelector('#playOff');
+    var rect2 = playoffSection.getBoundingClientRect();
+    console.log(rect2.top);
+    window.scroll({
+        top: rect2.top + window.scrollY, 
+        left: 0, 
+        behavior: 'smooth'
+    });
+};
+
+function scrollToPlayers() {
+    const playersSection = document.querySelector('#players');
+    var rect3 = playersSection.getBoundingClientRect();
+    console.log(rect3.top);
+    window.scroll({
+        top: rect3.top + window.scrollY, 
+        left: 0, 
+        behavior: 'smooth'
+    });
+};
 
