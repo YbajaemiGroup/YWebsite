0211c437453e0482ca08c77c40948b2548636f1a
devokius <carier1313@gmail.com>
2023-07-15 19:53:28 +0300

    Добавлено несколько ссылок на соц сети

--- website/core/YCore/Temporary/Program.cs
+++ website/core/YCore/Temporary/Program.cs
@@ -4,46 +4,5 @@ using YApi;
 using YApiModel.Models;
 
 var client = new YClient("token");
-<<<<<<< HEAD
-//var rounds = new List<Round>()
-//            {
-//                new()
-//                {
-//                    RoundNumber = 1,
-//                    IsUpper = true,
-//                    Games = new()
-//                    {
-//                        new()
-//                        {
-//                            Row = 1,
-//                            Player1Id = 1,
-//                            Player2Id = 2,
-//                            WinnerId = null
-//                        }
-//                    }
-//                },
-//                new()
-//                {
-//                    RoundNumber = 2,
-//                    IsUpper = true,
-//                    Games = new()
-//                    {
-//                        new()
-//                        {
-//                            Row = 1,
-//                            Player1Id = 1,
-//                            Player2Id = 2,
-//                            WinnerId = 1
-//                        }
-//                    }
-//                }
-//            };
-var bracket = client.GetBracketAsync().Result;
-
-Console.WriteLine(JsonSerializer.Serialize(bracket));
-=======
-
-client.PlayerDelete(1).Wait();
->>>>>>> console
 
 Console.ReadLine();
